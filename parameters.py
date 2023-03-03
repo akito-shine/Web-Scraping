f7ceb56bb7af0ae1b1a35eca22bc5c6d57367087
Akito <77329053+akito-shine@users.noreply.github.com>
2023-03-03 21:51:37 +0630

    Add files via upload

---
+++ parameters.py
@@ -0,0 +1,15 @@
+# search query
+# search_query = 'site:linkedin.com/in/ AND "ACCOUNTANCY" AND "Myanmar"'
+
+
+# file were scraped profile information will be stored
+file_name = 'results_file.csv'
+
+
+# login credentials
+fb_username = ''#add username
+
+
+
+# password
+fb_password = ''#add password
